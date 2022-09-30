01c20204d438f693d8572ba44a4e95e77a8593eb
vokur13 <vokur13@gmail.com>
2022-09-30 19:40:26 +0300

    project under construction

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import { Box } from '../components/Box';
-import { ContactForm } from '../components/ContactForm';
-import { Filter } from '../components/Filter';
-import { ContactList } from '../components/ContactList';
+import { Button } from '../components/Button';
+import { Modal } from '../components/Modal';
 import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    showModal: false,
   };
 
   formSubmitHandler = ({ name, number }) => {
@@ -32,27 +25,19 @@ export class App extends Component {
       : this.setState(({ contacts }) => ({
           contacts: [contact, ...contacts],
         }));
-
-    //     this.setState(({ contacts }) => ({
-    //       contacts: contacts.some(item =>
-    //         item.name.toLowerCase().includes(contact.name.toLowerCase())
-    //           ? alert(`${contact.name} is already in contacts`)
-    //           : [contact, ...contacts]
-    //       ),
-    //     }));
   };
 
   changeFilter = e => {
     this.setState({ filter: e.target.value });
   };
 
-  getFilteredItems = () => {
-    const { contacts, filter } = this.state;
-    const normilizedFilter = filter.toLowerCase();
-    return contacts.filter(item =>
-      item.name.toLowerCase().includes(normilizedFilter)
-    );
-  };
+  //   getFilteredItems = () => {
+  //     //     const { showModal } = this.state;
+  //     const normilizedFilter = filter.toLowerCase();
+  //     return contacts.filter(item =>
+  //       item.name.toLowerCase().includes(normilizedFilter)
+  //     );
+  //   };
 
   deleteItem = itemID => {
     this.setState(prevState => ({
@@ -60,18 +45,19 @@ export class App extends Component {
     }));
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
   render() {
-    const { filter } = this.state;
-    const filteredItem = this.getFilteredItems();
+    const { showModal } = this.state;
 
     return (
-      <Box width={1} p={4} bg="bgBasic" as="main">
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList onDelete={this.deleteItem} list={filteredItem} />
+      <Box as="main">
+        <button type="button" onClick={this.toggleModal}>
+          Open modal
+        </button>
+        {showModal && <Modal />}
       </Box>
     );
   }
